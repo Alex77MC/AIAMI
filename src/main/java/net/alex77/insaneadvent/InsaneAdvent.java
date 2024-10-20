ef6a320b3615975428f2d9c863c9eb0d9af4f0d8
Alex <aliego7194@gmail.com>
2024-10-19 22:08:56 -0700

    Enderite

--- src/main/java/net/alex77/insaneadvent/InsaneAdvent.java
+++ src/main/java/net/alex77/insaneadvent/InsaneAdvent.java
@@ -1,6 +1,8 @@
 package net.alex77.insaneadvent;
 
 import com.mojang.logging.LogUtils;
+import net.alex77.insaneadvent.item.ModItems;
+import net.minecraft.world.item.CreativeModeTabs;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.BuildCreativeModeTabContentsEvent;
@@ -34,6 +36,7 @@ public class InsaneAdvent
         // Register ourselves for server and other game events we are interested in
         MinecraftForge.EVENT_BUS.register(this);
 
+        ModItems.register(modEventBus);
         // Register the item to a creative tab
         modEventBus.addListener(this::addCreative);
 
@@ -49,7 +52,9 @@ public class InsaneAdvent
     // Add the example block item to the building blocks tab
     private void addCreative(BuildCreativeModeTabContentsEvent event)
     {
-
+        if (event.getTabKey() == CreativeModeTabs.INGREDIENTS) {
+            event.accept(ModItems.ENDERITE);
+        }
     }
 
     // You can use SubscribeEvent and let the Event Bus discover methods to call
